80808cdc216ff2f5d64b0255f721ad1ba5e18a59
Ayrton <ayrtton.vinicius@gmail.com>
2024-03-31 11:57:38 -0300

    Design the presentation text section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,8 +7,8 @@ function Main() {
         <main>
             <div className="service-presentation">
                 <div className="service-presentation__text">
-                    <h1 className="service-presentation__title">10,000+ of our users love our products.</h1>
-                    <p className="service-presentation__content">We only provide great products combined with 
+                    <h1 className="service-presentation__text-title">10,000+ of our users love our products.</h1>
+                    <p className="service-presentation__text-content">We only provide great products combined with 
                     excellent customer service. See what our satisfied customers are saying about our 
                     services.</p>
                 </div>
@@ -21,7 +21,7 @@ function Main() {
             <div className="service-reviews">
                 <div className="reviewer">
                     <div className="reviewer__profile">
-                        <img className="reviewer__profile-image" src={anne} alt="Anne image" />
+                        <img className="reviewer__profile-image" src={colton} alt="Colton image" />
                         <h2 className="reviewer__profile-name">Colton Smith</h2>
                         <span className="reviewer__profile-description">Verified Buyer</span>
                     </div>
@@ -31,7 +31,7 @@ function Main() {
                 </div>
                 <div className="reviewer">
                     <div className="reviewer__profile">
-                        <img className="reviewer__profile-image" src={colton} alt="Colton image" />
+                        <img className="reviewer__profile-image" src={irene} alt="Irene image" />
                         <h2 className="reviewer__profile-name">Irene Roberts</h2>
                         <span className="reviewer__profile-description">Verified Buyer</span>
                     </div>
@@ -41,13 +41,13 @@ function Main() {
                 </div>
                 <div className="reviewer">
                     <div className="reviewer__profile">
-                        <img className="reviewer__profile-image" src={irene} alt="" />
+                        <img className="reviewer__profile-image" src={anne} alt="Anne image" />
                         <h2 className="reviewer__profile-name">Anne Wallace</h2>
                         <span className="reviewer__profile-description">Verified Buyer</span>
                     </div>
                     <p className="reviewer__text">{'"'}Put an order with this company and can only praise 
                     them for the very high standard. Will definitely use them again and recommend them to 
-                    everyone!{'"'}</p>
+                    8everyone!{'"'}</p>
                 </div>
             </div>
         </main>
